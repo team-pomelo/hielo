c05790a4d728bca72ce623ebe3eebf9dd28704b5
Marco Ceppi <marco@ceppi.net>
2013-12-23 08:27:06 -0500

    Dynamically update the navigation based on page URL

--- assets/linker/js/global.js
+++ assets/linker/js/global.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
   $('a[data-toggle="tooltip"]').tooltip();
+  $('div[role="navigation"] ul.nav.navbar-nav li > a[href="' + document.location.pathname + '"]').parent().addClass('active');
 });
